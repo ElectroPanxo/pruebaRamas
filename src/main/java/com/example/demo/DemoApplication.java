366735b5b3e66561ca98c82d5294204a3810fa17
ElectroPanxo <juanmanuelrubialesdelacruz@gmail.com>
2022-01-13 09:03:09 +0100

    finalizacion

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,8 +8,11 @@ public class DemoApplication {
 		System.out.println("Rama Master");
 		System.out.println("Esto es un mensaje de inicio de la rama Master");
 		System.out.println("Esto es un mensaje de la rama Master");
-		System.out.println("Esto es un mensaje de Mordekaiser");
 		System.out.println("Esto es un mensaje nuevo de la rama Master");
+
+		System.out.println(" ------------------------ ");
+
+		System.out.println("Esto es un mensaje de Mordekaiser");
 		System.out.println("Esto es un mensaje de Gwen");
 	}
 
