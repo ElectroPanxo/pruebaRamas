edf391aa982ae623d56ae080175e49d0d7f0be5c
ElectroPanxo <juanmanuelrubialesdelacruz@gmail.com>
2022-01-13 08:46:18 +0100

    mensajeCreador

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,8 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		System.out.println("Rama Master");
 		System.out.println("Esto es un mensaje de la rama Master");
+		System.out.println("Rama Creador");
+		System.out.println("Esto es un mensaje de la rama Creador");
 	}
 
 }
