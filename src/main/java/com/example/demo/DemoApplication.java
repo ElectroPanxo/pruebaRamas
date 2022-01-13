9b5fd6874906458cd0bbd7b95906c6bacb0316e1
ElectroPanxo <juanmanuelrubialesdelacruz@gmail.com>
2022-01-13 08:58:16 +0100

    mensajeDeInicio

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Rama Master");
+		System.out.println("Esto es un mensaje de inicio de la rama Master");
 		System.out.println("Esto es un mensaje de la rama Master");
 		System.out.println("Esto es un mensaje nuevo de la rama Master");
 	}
