c9ce9c994aeb344c79aa0c8dfda706778570a9e7
ElectroPanxo <juanmanuelrubialesdelacruz@gmail.com>
2022-01-13 08:55:35 +0100

    mensajeMordekaiser

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		System.out.println("Rama Master");
 		System.out.println("Esto es un mensaje de la rama Master");
+		System.out.println("Esto es un mensaje de Mordekaiser");
 	}
 
 }
